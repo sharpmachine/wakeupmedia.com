505ee9256225f85c02af1124173c519af8d3bd79
Jesse Kade <jessekade@gmail.com>
2011-10-15 17:45:08 -0700

    changes h3 tag on disqus

--- wp-content/themes/wakeup/header.php
+++ wp-content/themes/wakeup/header.php
@@ -39,8 +39,6 @@
 	?></title>
 	<meta name="author" content="Jesse Kade of Sharp Machine Media">
 	
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/icons/favicon.ico">
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/images/icons/apple-touch-icon.png">
 	
