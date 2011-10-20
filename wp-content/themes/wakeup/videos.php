c80ca4ddf2a81e61db822fff85487d282e40d527
Jesse Kade <jessekade@gmail.com>
2011-10-19 19:05:09 -0700

    working on videos page some more

--- wp-content/themes/wakeup/videos.php
+++ wp-content/themes/wakeup/videos.php
@@ -6,15 +6,30 @@
 
 		<div id="content-container">
 			<section id="content" role="main">	
-			<?php get_template_part( 'loop', 'page' ); ?>
-			<?php rewind_posts(); ?>
+					<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
+					
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php if ( is_front_page() ) { ?>
+						<h2 class="entry-title"><?php the_title(); ?></h2>
+					<?php } else { ?>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php } ?>
+					
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+					
+	<?php endwhile; endif; ?>
+	
 				<div class="yt_holder">
     				<div id="ytvideo"></div>
 					<ul class="video-playlist">
 						<?php query_posts("post_type=videos&posts_per_page=100"); ?>
 						<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
 						<li>
-							<a class="video-thumb" href="http://www.youtube.com/watch?v=<?php the_field('youtube_video_id'); ?>"></a>
+							<a class="video-thumb" href="http://www.youtube.com/watch?v=<?php the_field('youtube_video_id'); ?>"><br \><?php the_title(); ?></a>
 							
 						</li>
 						<?php endwhile; ?>
